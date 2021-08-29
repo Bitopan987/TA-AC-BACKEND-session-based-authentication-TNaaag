4e4be38422826368463e4d0e9f73ace1abc2ead5
bitopan987 <bitopanxon1@gmail.com>
2021-08-29 17:57:58 +0530

    completed logout form

---
+++ block-BNaadt/user-logout/routes/users.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/User');
+
+/* GET users listing. */
+
+router.get('/', function (req, res, next) {
+  console.log(req.session);
+  res.render('users');
+});
+
+router.get('/register', function (req, res, next) {
+  res.render('register');
+});
+
+router.post('/register', (req, res, next) => {
+  User.create(req.body, (err, user) => {
+    if (err) return next(err);
+    res.redirect('/users/login');
+  });
+});
+
+router.get('/login', (req, res, next) => {
+  var error = req.flash('error')[0];
+  res.render('login', { error });
+});
+
+router.post('/login', (req, res, next) => {
+  var { email, password } = req.body;
+  if (!email || !password) {
+    req.flash('error', 'Email/Password required');
+    return res.redirect('/users/login');
+  }
+  User.findOne({ email }, (err, user) => {
+    if (err) return next(err);
+    // no user
+    if (!user) {
+      req.flash('error', 'This email is not registered');
+      return res.redirect('/users/login');
+    }
+    // compare password
+    user.verifyPassword(password, (err, result) => {
+      if (err) return next(err);
+      if (!result) {
+        req.flash('error', 'Invalid Password');
+        return res.redirect('/users/login');
+      }
+      // persist login user info
+      req.session.userId = user.id;
+      res.render('dashBoard');
+    });
+  });
+});
+
+router.get('/logout', (req, res, next) => {
+  req.session.destroy();
+  res.clearCookie('connect.sid');
+  res.redirect('/users/login');
+});
+
+module.exports = router;
